c671dfa069852fc2491e27cec9294c22a952258c
Draculk <kayranvieira456@hotmail.com>
2021-04-22 16:40:12 -0300

    :sparkles: delete e check funcionando.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const todoList = document.querySelector('.todo-list');
 
 //Event Listeners
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
 
 //Functions
 function addTodo(event){
@@ -40,4 +41,19 @@ function addTodo(event){
 
     //Limpar o input do todo
     todoInput.value = "";
+}
+
+function deleteCheck(e){
+    const item = e.target;
+    //Delete button
+    if(item.classList[0] === 'trash-btn'){
+        const todo = item.parentElement;
+        todo.remove();
+    }
+
+    //Check button
+    if(item.classList[0] === 'complete-btn'){
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
 }
